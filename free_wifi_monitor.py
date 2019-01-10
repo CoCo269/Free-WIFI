8d21f958f638d30a6c1375316de6f5d2b4838fd8
Cozak <sscozak@gmail.com>
2019-01-11 02:31:58 +0800

    [修改] 修正监控程序不检查状态文件 UUID 是否匹配的问题 [修改] 完善监控程序的日志输出

--- free_wifi_monitor.py
+++ free_wifi_monitor.py
@@ -1,13 +1,13 @@
-import os, re, time
+import os, sys, re, time
 from free_wifi_basekits import *
 from free_wifi_config import *
 
 WIFI_CONFIG = WIFI_CONFIG
 
-def isRunning():
+def isRunning(state):
 	# 当前路径下
 	return bool(SearchFilesInCondition(path=".", 
-		cond=(lambda obj:bool(obj.find(WIFI_CONFIG["monitor"]["prefix"]) >= 0))))
+		cond=(lambda obj:bool(obj.find(state) >= 0))))
 
 def checkAbort():
 	res = "".join(os.popen("netsh wlan show hostednetwork").readlines())
@@ -17,9 +17,13 @@ def checkAbort():
 def startWIFI():
 	os.system("netsh wlan start hostednetwork")
 
-while isRunning():
-	if checkAbort():
-		startWIFI()
-	time.sleep(WIFI_CONFIG["monitor"]["interval"])
+if __name__ == '__main__':
 
-LogN("Free wifi monitor stop !!!")
+	state = sys.argv[1]
+	while isRunning(state):
+		if checkAbort():
+			LogN("Wifi monitor for {state} is retrying ...".format(state=state))
+			startWIFI()
+		time.sleep(WIFI_CONFIG["monitor"]["interval"])
+
+	LogN("Wifi monitor for {state} stop !!!".format(state=state))
